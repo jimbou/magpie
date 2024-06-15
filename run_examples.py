d05548b12981df624ba894002a542229dff963d1
jimbou <el17072@mail.ntua.gr>
2024-06-15 13:20:42 +0100

    sat4j params

--- run_examples.py
+++ run_examples.py
@@ -192,7 +192,8 @@ def main(name1, scenario ,name3, compile_command, improved_file, main_directory,
     perf_items = ['time','perf_time','perf_instructions', 'perf_cycles',
         "perf_cache_references", "perf_cache_misses", "perf_branches",
         "perf_branch_misses", "perf_cpu_clock", "perf_task_clock", "perf_faults", "weights", "energy"]
-    perf_items = [ 'time','perf_time','perf_instructions', 'perf_cycles']
+    perf_items = [ "perf_cache_references", "perf_cache_misses", "perf_branches",
+        "perf_branch_misses", "perf_cpu_clock"]
     erroneous=[]
     execution_times = []
     run_com =name3
