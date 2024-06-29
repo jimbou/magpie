62385760cc62ef8a58b5633d073f3c3cdcc8c319
jimbou <el17072@mail.ntua.gr>
2024-06-29 21:05:34 +0100

    prepared final initial calculations

--- new.sh
+++ new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd examples/sat4j/necessary/
+cd examples/scipy
 ./total.sh "bash run_fixed.sh"
 
-cd ../../
-cd lpg/necessary
-./total.sh "bash run_fixed.sh"
+cd ../
+cd zlib
+./total.sh "bash run_fixed.sh level=6 wbits=15 memLevel=8 strategy=0"
