95d7f3130b56da1acb2929c35a1ae6f5fcc535c7
jimbou <jimbou1999@gmail.com>
2024-07-15 14:27:00 +0100

    test commit automation

--- git.sh
+++ git.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Path to your SSH key
 # Absolute path to your SSH key
 SSH_KEY_PATH="/home/dbouras/.ssh/id_rsa"
 
@@ -24,21 +23,35 @@ else
     echo "Failed to add SSH key"
     exit 1
 fi
-# Ensure the SSH agent is running and the key is added
-eval "$(ssh-agent -s)"
-ssh-add "$SSH_KEY_PATH"
 
 # Configure Git user information
 git config --global user.email "jimbou1999@gmail.com"
 git config --global user.name "jimbou"
+
+# Ensure the remote URL is using SSH
+git remote set-url origin git@github.com:jimbou/magpie.git
+
+# Other commands before git phase
+echo "Executing commands before git phase"
 cp today.sh today23.sh
 
+# Git phase
+echo "Starting git phase"
+
+# Pull the latest changes
 git pull || true
+
 # Add changes to staging area
 git add . || true
+
 # Commit the changes
 git commit -m "test commit automation" || true
-# Push the changes
-git push origin|| true
 
+# Push the changes
+git push origin || true
+
+echo "Git phase completed"
+
+# Other commands after git phase
+echo "Executing commands after git phase"
 cp today.sh today24.sh
