c66549e2573b59001ccf546fc1328bd48bdf79a6
Dimitrios BOURAS <dbouras@crouch.cs.ucl.ac.uk>
2024-07-15 10:11:10 +0100

    minisat hack

--- today.sh
+++ today.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-python3.11 run_examples2.py minisat "" scenario_runtime_config1 "bash run_fixed.sh" "" minisat_simplified.params minisat_simplified.params genetic_programming
-sleep 100
-python3.11 run_examples.py minisat "" scenario_runtime_config3 "bash run_fixed.sh" "" minisat_simplified.params minisat_simplified.params local_search
-sleep 1800
-python3.11 run_examples.py minisat "" scenario_runtime_config3 "bash run_fixed.sh" "" minisat_simplified.params minisat_simplified.params genetic_programming
-
+cd minisat_run11
+python3 solve_problem.py 
+cd ../
+sleep 60
+python3.11 run_examples_2.py minisat "" scenario_runtime_config3 "bash run_fixed.sh" "bash compile.sh" core/Solver.cc "" genetic_programming
+sleep 600
+python3.11 run_examples.py minisat_hack "" scenario_runtime_config1 "bash run_fixed.sh" "" minisat_advanced.params minisat_advanced.params local_search
+sleep 600
+python3.11 run_examples.py minisat_hack "" scenario_runtime_config3 "bash run_fixed.sh" "./build.sh" sources/core/Solver.cc "" local_search
